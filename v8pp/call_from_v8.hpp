212ec78e2e49814057c0db010ab8213c4213010a
Pavel Medvedev <pmedvedev@gmail.com>
2013-09-12 13:47:58 +0400

    Fixing Visual C++ 2010 warning C4100 in v8pp

--- v8pp/call_from_v8.hpp
+++ v8pp/call_from_v8.hpp
@@ -80,6 +80,7 @@ struct call_from_v8_helper_function<P, n>
 	exec(typename P::function_type ptr, v8::Arguments const& args)
 	{
 		typedef typename P::arguments args_tl;
+		args;
 		return (*ptr)(BOOST_PP_ENUM(n, V8PP_CALL_V8_from_v8_args, ~));
 	}
 };
@@ -91,6 +92,7 @@ struct call_from_v8_helper_mem_function<P, T, n>
 	exec(T& obj, typename P::method_type ptr, v8::Arguments const& args)
 	{
 		typedef typename P::arguments args_tl;
+		args;
 		return (obj.*ptr)(BOOST_PP_ENUM(n, V8PP_CALL_V8_from_v8_args, ~));
 	}
 };
