8bd58188b5ca748344dcfd06bba8927bb0da0bf1
Pavel Medvedev <pmedvedev@gmail.com>
2013-11-16 10:46:29 +0400

    Adding from_v8() function overloading that returns a default value in case of conversion error
    Conflicts:
    	v8pp/from_v8.hpp

--- v8pp/from_v8.hpp
+++ v8pp/from_v8.hpp
@@ -15,11 +15,11 @@ namespace detail {
 template<typename T>
 T get_object_field(v8::Handle<v8::Value> value)
 {
-	while ( value->IsObject() )
+	while (value->IsObject())
 	{
 		v8::Local<v8::Object> obj = value->ToObject();
 		T native = reinterpret_cast<T>(obj->GetAlignedPointerFromInternalField(0));
-		if ( native )
+		if (native)
 		{
 			return native;
 		}
@@ -45,14 +45,19 @@ struct from_v8<std::string>
 {
 	typedef std::string result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type const& def_value)
+	{
+		return value->IsString()? exec(value) :  def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsString() )
+		if (value->IsString())
 		{
-			throw std::runtime_error("cannot make string from non-string type");
+			v8::String::Utf8Value const utf8_str(value);
+			return result_type(*utf8_str, utf8_str.length());
 		}
-		v8::String::Utf8Value const utf8_str(value);
-		return result_type(*utf8_str, utf8_str.length());
+		throw std::runtime_error("expected String");
 	}
 };
 
@@ -61,14 +66,19 @@ struct from_v8<convertible_string<char>>
 {
 	typedef convertible_string<char> result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type const& def_value)
+	{
+		return value->IsString()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsString() )
+		if (value->IsString())
 		{
-			throw std::runtime_error("cannot make string from non-string type");
+			v8::String::Utf8Value const utf8_str(value);
+			return result_type(*utf8_str, utf8_str.length());
 		}
-		v8::String::Utf8Value const utf8_str(value);
-		return result_type(*utf8_str, utf8_str.length());
+		throw std::runtime_error("expected String");
 	}
 };
 
@@ -88,14 +98,19 @@ struct from_v8<std::wstring>
 {
 	typedef std::wstring result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type const& def_value)
+	{
+		return value->IsString()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsString() )
+		if (value->IsString())
 		{
-			throw std::runtime_error("cannot make string from non-string type");
+			v8::String::Value const utf16_str(value);
+			return result_type((wchar_t const*)(*utf16_str), utf16_str.length());
 		}
-		v8::String::Value const utf16_str(value);
-		return result_type((wchar_t const*)(*utf16_str), utf16_str.length());
+		throw std::runtime_error("expected String");
 	}
 };
 
@@ -104,14 +119,19 @@ struct from_v8<convertible_string<wchar_t>>
 {
 	typedef convertible_string<wchar_t> result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type const& def_value)
+	{
+		return value->IsString()? exec(value) :def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsString() )
+		if (value->IsString())
 		{
-			throw std::runtime_error("cannot make string from non-string type");
+			v8::String::Value const utf16_str(value);
+			return result_type((wchar_t const*)(*utf16_str), utf16_str.length());
 		}
-		v8::String::Value const utf16_str(value);
-		return result_type((wchar_t const*)(*utf16_str), utf16_str.length());
+		throw std::runtime_error("expected String");
 	}
 };
 
@@ -127,13 +147,18 @@ struct from_v8< v8::Handle<v8::Function> >
 {
 	typedef v8::Handle<v8::Function> result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsFunction()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsFunction() )
+		if (value->IsFunction())
 		{
-			throw std::runtime_error("expected javascript function");
+			return value.As<v8::Function>();
 		}
-		return value.As<v8::Function>();
+		throw std::runtime_error("expected Function");
 	}
 };
 
@@ -141,6 +166,11 @@ template<>
 struct from_v8<bool>
 {
 	typedef bool result_type;
+
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsBoolean()? exec(value) : def_value;
+	}
 
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
@@ -153,14 +183,18 @@ struct from_v8<int32_t>
 {
 	typedef int32_t result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsNumber()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsNumber() )
+		if (value->IsNumber())
 		{
-			throw std::runtime_error("expected javascript number");
+			return value->ToInt32()->Value();
 		}
-
-		return value->ToInt32()->Value();
+		throw std::runtime_error("expected Number");
 	}
 };
 
@@ -169,14 +203,18 @@ struct from_v8<uint32_t>
 {
 	typedef uint32_t result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsNumber()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsNumber() )
+		if (value->IsNumber())
 		{
-			throw std::runtime_error("expected javascript number");
+			return value->ToUint32()->Value();
 		}
-
-		return value->ToUint32()->Value();
+		throw std::runtime_error("expected Number");
 	}
 };
 
@@ -185,13 +223,18 @@ struct from_v8<int64_t>
 {
 	typedef int64_t result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsNumber()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsNumber() )
+		if (value->IsNumber())
 		{
-			throw std::runtime_error("expected javascript number");
+			return static_cast<result_type>(value->ToNumber()->Value());
 		}
-		return static_cast<result_type>(value->ToNumber()->Value());
+		throw std::runtime_error("expected Number");
 	}
 };
 
@@ -200,13 +243,38 @@ struct from_v8<uint64_t>
 {
 	typedef uint64_t result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsNumber()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsNumber() )
+		if (value->IsNumber())
 		{
-			throw std::runtime_error("expected javascript number");
+			return static_cast<result_type>(value->ToNumber()->Value());
 		}
-		return static_cast<result_type>(value->ToNumber()->Value());
+		throw std::runtime_error("expected Number");
+	}
+};
+
+template<>
+struct from_v8<float>
+{
+	typedef float result_type;
+
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsNumber()? exec(value) : def_value;
+	}
+
+	static result_type exec(v8::Handle<v8::Value> value)
+	{
+		if (value->IsNumber())
+		{
+			return static_cast<float>(value->ToNumber()->Value());
+		}
+		throw std::runtime_error("expected Number");
 	}
 };
 
@@ -215,13 +283,18 @@ struct from_v8<double>
 {
 	typedef double result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsNumber()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsNumber() )
+		if (value->IsNumber())
 		{
-			throw std::runtime_error("expected javascript number");
+			return value->ToNumber()->Value();
 		}
-		return value->ToNumber()->Value();
+		throw std::runtime_error("expected Number");
 	}
 };
 
@@ -245,22 +318,26 @@ struct from_v8< std::vector<T, A> >
 {
 	typedef std::vector<T, A> result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type const& def_value)
+	{
+		return value->IsArray()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsArray() )
+		if (value->IsArray())
 		{
-			throw std::runtime_error("expected javascript array");
-		}
+			v8::Array* array = v8::Array::Cast(*value);
 
-		v8::Array* array = v8::Array::Cast(*value);
-
-		result_type result;
-		result.reserve(array->Length());
-		for (uint32_t i = 0, count = array->Length(); i < count; ++i)
-		{
-			result.push_back(from_v8<T>::exec(array->Get(i)));
+			result_type result;
+			result.reserve(array->Length());
+			for (uint32_t i = 0, count = array->Length(); i < count; ++i)
+			{
+				result.push_back(from_v8<T>::exec(array->Get(i)));
+			}
+			return result;
 		}
-		return result;
+		throw std::runtime_error("expected Array");
 	}
 };
 
@@ -268,6 +345,11 @@ template<>
 struct from_v8< v8::Handle<v8::Value> >
 {
 	typedef v8::Handle<v8::Value> result_type;
+
+	static result_type exec(v8::Handle<v8::Value> value, v8::Handle<v8::Value> def_value)
+	{
+		return value.IsEmpty()? def_value : value;
+	}
 
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
@@ -282,19 +364,22 @@ struct from_v8_ptr
 {
 	typedef T* result_type;
 
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsObject()? exec(value) : def_value;
+	}
+
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsObject() )
+		if (value->IsObject())
 		{
-			return nullptr;
+			if (T* obj_ptr = get_object_field<T*>(value))
+			{
+				return obj_ptr;
+			}
+			throw std::runtime_error(std::string("expected C++ wrapped object ") + typeid(T).name());
 		}
-
-		T* obj_ptr = get_object_field<T*>(value);
-		if ( !obj_ptr )
-		{
-			throw std::runtime_error("expected C++ wrapped object");
-		}
-		return obj_ptr;
+		return nullptr;
 	}
 };
 
@@ -309,22 +394,23 @@ struct from_v8<T const *> : from_v8_ptr<T const> {};
 template<typename T, typename U>
 struct from_v8_ref
 {
-
 	typedef U result_type;
+
+	static result_type exec(v8::Handle<v8::Value> value, result_type def_value)
+	{
+		return value->IsObject()? exec(value) : def_value;
+	}
 
 	static result_type exec(v8::Handle<v8::Value> value)
 	{
-		if ( !value->IsObject() )
+		if (value->IsObject())
 		{
-			throw std::runtime_error("expected object");
+			if (T* obj_ptr = get_object_field<T*>(value))
+			{
+				return *obj_ptr;
+			}
 		}
-
-		T* obj_ptr = get_object_field<T*>(value);
-		if ( !obj_ptr )
-		{
-			throw std::runtime_error("expected C++ wrapped object");
-		}
-		return *obj_ptr;
+		throw std::runtime_error(std::string("expected C++ wrapped object ") + typeid(T).name());
 	}
 };
 
@@ -350,6 +436,12 @@ template<typename T>
 inline typename detail::from_v8<T>::result_type from_v8(v8::Handle<v8::Value> src)
 {
 	return detail::from_v8<T>::exec(src);
+}
+
+template<typename T>
+inline typename detail::from_v8<T>::result_type from_v8(v8::Handle<v8::Value> src, T const& def_value)
+{
+	return detail::from_v8<T>::exec(src, def_value);
 }
 
 } // namesapce v8pp
