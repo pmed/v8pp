d4bd5668f813e33014116a4718655b409a0aa624
Pavel Medvedev <pmedvedev@gmail.com>
2015-06-06 15:06:33 +0300

    Add build matrix for Travis CI

--- build-v8.sh
+++ build-v8.sh
@@ -7,7 +7,7 @@ cd v8
 gclient sync
 export CXXFLAGS="-Wno-unknown-warning-option"
 export GYPFLAGS="-Dv8_use_external_startup_data=0"
-make native library=shared werror=no -j8
+make native werror=no library=shared -j8
 mkdir -p lib
 cp out/native/lib.target/*.so lib
 cp out/native/obj.target/tools/gyp/lib*.a lib
