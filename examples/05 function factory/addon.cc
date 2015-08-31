fbd07266dd0695032f3877e4fdcbc3c0e2821eea
Pavel Medvedev <pmedvedev@gmail.com>
2015-08-31 20:21:55 +0300

    Add copyright information

--- examples/05 function factory/addon.cc
+++ examples/05 function factory/addon.cc
@@ -1,3 +1,11 @@
+//
+// Copyright (c) 2013-2015 Pavel Medvedev. All rights reserved.
+//
+// This file is part of v8pp (https://github.com/pmed/v8pp) project.
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <node.h>
 #include <v8pp/function.hpp>
 #include <v8pp/object.hpp>
