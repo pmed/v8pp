8a121d4c21b968b399c3f4293f94cde633308442
William Lohry <wflohry@gmail.com>
2021-03-02 21:15:02 +0100

    add `v8pp::convert<std::variant>` and tests
    Squashed commits:
    
    Updating type traits
    
    Support for std::map
    
    Moving explicit template specialization to namespace scope
    
    fix standard containers printing in tests
    
    Using overloaded set of `operator<<()` for different containers.
    
    Modifying integer check to be compatible with v8
    
    Merged updates to master branch
    
    Fixing implicit conversion error
    
    Better handling of numeric limits and double conversion for integer types.
    
    Removing extraneous template parameter
    
    Adding validType array
    
    Ensuring wrong types throw
    
    Simplified value check
    
    Added check for integral types
    
    Adding variant and tests

--- test/test_convert.cpp
+++ test/test_convert.cpp
@@ -8,6 +8,7 @@
 //
 #include "v8pp/convert.hpp"
 #include "test.hpp"
+#include "v8pp/class.hpp"
 
 #include <list>
 #include <vector>
@@ -127,6 +128,146 @@ struct convert<person>
 
 } // v8pp
 
+namespace {
+struct U {
+	U(int value = 1) : value(value) {}
+	int value = 1;
+	bool operator==(const U& other) const {
+		return value == other.value;
+	}
+	friend std::ostream& operator<<(std::ostream& os, U const& val)
+	{
+		return os << val.value;
+	}
+};
+struct U2 {
+	U2(double value = 2.) : value(value) {}
+	double value = 2.;
+	bool operator==(const U2& other) const {
+		return value == other.value;
+	}
+	friend std::ostream& operator<<(std::ostream& os, U2 const& val)
+	{
+		return os << val.value;
+	}
+};
+struct V {
+	V(std::string value = "") : value(value) {}
+	std::string value = "test";
+	bool operator==(const V& other) const {
+		return value == other.value;
+	}
+	friend std::ostream& operator<<(std::ostream& os, V const& val)
+	{
+		return os << val.value;
+	}
+};
+struct V2 {
+	V2(std::string value = "") : value(value) {}
+	std::string value = "test";
+	bool operator==(const V2& other) const {
+		return value == other.value;
+	}
+	friend std::ostream& operator<<(std::ostream& os, V2 const& val)
+	{
+		return os << val.value;
+	}
+};
+
+template <typename T> struct VariantCheck {};
+
+template <typename ... Ts>
+struct VariantCheck<std::variant<Ts...>> {
+	VariantCheck(v8::Isolate * isolate) : isolate(isolate) {}
+	v8::Isolate * isolate;
+	using Variant = std::variant<Ts...>;
+
+	template <typename T>
+	static T get(const T& in){
+		return in;
+	}
+	template <typename T>
+	static T get(const std::variant<Ts...> &in){
+		return std::get<T>(in);
+	}
+
+	template <typename T, typename From, typename To>
+	VariantCheck& check(const T &value)
+	{
+		From values = value;
+		auto local = v8pp::convert<From>::to_v8(isolate, values);
+		auto back = v8pp::convert<To>::from_v8(isolate, local);
+		T returned = VariantCheck::get<T>(back);
+		::check(v8pp::detail::type_id<Variant>().name(), returned == value);
+		return *this;
+	}
+
+	template <typename T>
+	VariantCheck& checkValue(T value)
+	{
+		check<T, Variant, Variant>(value); // variant to variant
+		check<T, Variant, T>(value); // variant to type
+		check<T, T, Variant>(value); // type to variant
+		return *this;
+	}
+
+	template <typename T>
+	VariantCheck& checkThrow(T value){
+		auto local = v8pp::convert<T>::to_v8(isolate, value);
+		check_ex<std::exception>("variant", [&]{
+			v8pp::convert<Variant>::from_v8(isolate, local);
+		});
+		return *this;
+	}
+
+	VariantCheck& operator()(std::tuple<Ts...> && values)
+	{
+		(checkValue<Ts>(std::get<Ts>(values)),...);
+		return *this;
+	}
+
+};
+
+template <typename T>
+constexpr std::pair<T, T> getLimits()
+{
+	return { std::numeric_limits<T>::min(), std::numeric_limits<T>::max() };
+}
+
+template <>
+constexpr std::pair<int64_t, int64_t> getLimits<int64_t>()
+{
+	constexpr int maxInt = 0x7FFFFFFF;
+	return { -maxInt - 1, maxInt };
+}
+
+template <>
+constexpr std::pair<uint64_t, uint64_t> getLimits<uint64_t>()
+{
+	return { 0, 0xFFFFFFFFu };
+}
+
+template <typename T>
+void checkRange(v8::Isolate * isolate)
+{
+	using Variant = std::variant<T>;
+	VariantCheck<Variant> check{isolate};
+	constexpr std::pair<T, T> limits = getLimits<T>();
+	check(T(0));
+	check(std::tuple<T>{limits.first});
+	check(std::tuple<T>{limits.second});
+	check.template checkThrow<double>(std::nextafter(double(limits.first), std::numeric_limits<double>::min())); // like min - 1 (out of range)
+	check.template checkThrow<double>(std::nextafter(double(limits.second), std::numeric_limits<double>::max())); // like max + 1 (out of range)
+}
+
+
+template <typename ... Ts>
+void checkRanges(v8::Isolate * isolate)
+{
+	(checkRange<Ts>(isolate),...);
+}
+} // namespace
+
 void test_convert()
 {
 	v8pp::context context;
@@ -201,4 +342,60 @@ void test_convert()
 		v8::Local<v8::Array> tuple_1_ = v8pp::to_v8(isolate, tuple_1);
 		v8pp::from_v8<std::tuple<size_t, std::string>>(isolate, tuple_1_);
 	});
+
+	// Variant check
+	v8pp::class_<U, v8pp::raw_ptr_traits> U_class(isolate);
+	U_class.template ctor<>().auto_wrap_objects(true);
+	v8pp::class_<U2, v8pp::raw_ptr_traits> U2_class(isolate);
+	U2_class.template ctor<>().auto_wrap_objects(true);
+	v8pp::class_<V, v8pp::shared_ptr_traits> V_class(isolate);
+	V_class.template ctor<>().auto_wrap_objects(true);
+	v8pp::class_<V2, v8pp::shared_ptr_traits> V2_class(isolate);
+	V2_class.template ctor<>().auto_wrap_objects(true);
+	auto V_ = std::make_shared<V>(V{"test" });
+	auto V2_ = std::make_shared<V2>(V2{"test2"});
+	V_class.reference_external(isolate, V_);
+	V2_class.reference_external(isolate, V2_);
+
+	using Variant = std::variant<U, std::shared_ptr<V>, int, std::string, U2, std::shared_ptr<V2>>;
+	using ArithmeticVariant = std::variant<bool, float, int32_t>;
+	using ArithmeticVariantReversed = std::variant<int32_t, float, bool>;
+	using VariantVector = std::variant<std::vector<float>, float, std::string>;
+
+	VariantCheck<Variant> check{isolate};
+	check({U{2}, V_, -1, std::string("Hello"), U2{3.}, V2_});
+
+	VariantCheck<ArithmeticVariant> checkArithmetic(context.isolate());
+	checkArithmetic({bool{true}, float{5.5f}, int32_t{2}});
+
+	VariantCheck<ArithmeticVariantReversed> checkArithmeticReversed(context.isolate());
+	checkArithmeticReversed({int32_t{2}, float{5.5f}, bool{true}});
+
+	VariantCheck<VariantVector> checkVector(context.isolate());
+	checkVector({std::vector<float>{1.f, 2.f, 3.f}, float{4.f}, std::string{"testing"}});
+
+	// The order here matters
+	using ValueChecking = std::variant<int8_t, uint8_t, int32_t, uint32_t, double>;
+	const double largeNumber = static_cast<double>(std::numeric_limits<uint32_t>::max()) + 1.;
+	VariantCheck<ValueChecking>{context.isolate()}({-1, 254, std::numeric_limits<int32_t>::min(), std::numeric_limits<uint32_t>::max(), largeNumber});
+
+	using SimpleArithmetic = std::variant<bool, int8_t>;
+	VariantCheck<SimpleArithmetic> simpleArithmetic{context.isolate()};
+	simpleArithmetic.checkThrow(std::numeric_limits<uint32_t>::max()) // does not fit into int8_t
+					.checkThrow(1.5) // is not integral
+					.checkThrow(V_); // is not arithmetic
+
+	using ObjectsOnly = std::variant<U, std::shared_ptr<V>, std::vector<float>>;
+	VariantCheck<ObjectsOnly> objectsOnly{context.isolate()};
+	objectsOnly.checkThrow(true)
+		.checkThrow("test")
+		.checkThrow(1.);
+
+	// Note: Not all values of uint64_t/int64_t are possible since v8 stores numeric values as doubles
+	checkRanges<int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t>(isolate);
+
+	// test map
+	using MapVariant = std::variant<U, std::map<size_t, U>>;
+	VariantCheck<MapVariant> mapCheck{isolate};
+	mapCheck({U{3}, std::map<size_t, U>{{4, U{4}}}});
 }
