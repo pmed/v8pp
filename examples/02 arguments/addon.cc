79fcaf65ac28bd0999b8d6e8e6a86062d63922d8
Pavel Medvedev <pmedvedev@gmail.com>
2021-10-18 18:30:52 +0200

    add copyright and license for original vu8 project
    remove useless license headers from the source code
    
    (cherry picked from commit 62d9fb847035dcdffd58bc07eb20a1b4c1ad100b)

--- examples/02 arguments/addon.cc
+++ examples/02 arguments/addon.cc
@@ -1,11 +1,3 @@
-//
-// Copyright (c) 2013-2016 Pavel Medvedev. All rights reserved.
-//
-// This file is part of v8pp (https://github.com/pmed/v8pp) project.
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
 #include <node.h>
 #include <v8pp/module.hpp>
 
